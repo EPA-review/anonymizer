19d233c562295f4f85f42cf2e39c5375eb5f5eac
Sean Wong <seanwong24@live.com>
2021-10-04 20:47:19 -0600

    add test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from analyzer import analyzeText, serializeList
 
-text = 'Tom thinks this is a great idea.'
+text = 'Tom thinks this isn\'t a great idea.'
 names = ['Tom']
 result = serializeList(analyzeText(text, names))
 print(result)
