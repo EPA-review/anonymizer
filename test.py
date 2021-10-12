25dc6c4bc586146e5b195e47f6b658e5ad2389a8
Sean Wong <seanwong24@live.com>
2021-10-12 15:45:50 -0600

    update test.py

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
+from typing import List, NamedTuple
 from analyzer import analyzeText, serializeList
 
-text = 'Tom thinks this isn\'t a great idea. D/W.'
-names = ['Tom']
-result = serializeList(analyzeText(text, names))
-print(result)
+TestCase = NamedTuple('Test', [('text', str), ('names', List[str])])
+
+tests: List[TestCase] = [
+    TestCase('Tom thinks this isn\'t a great idea. D/W.', ['Tom']),
+    TestCase('Managed the resp distress, approp ddx, proper interp of ABG, Discussed disposition, tx', ['Someone'])
+]
+for test in tests:
+    text = test.text
+    names = test.names
+    result = serializeList(analyzeText(text, names))
+    print(result)
