d69f28dd3069960cc8edd30c4f2592cb18c48647
Sean Wong <seanwong24@live.com>
2021-12-06 02:05:07 -0600

    use nicknames.json

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from typing import List, NamedTuple
 from SimpleAnonymizer import AnonymizeText
+from flask import json
 
 TestCase = NamedTuple('Test', [('text', str), ('names', List[str])])
 
@@ -10,12 +11,11 @@ tests: List[TestCase] = [
     TestCase('EPA completed by Dr. A. McConnell on ID rotation \r\nSubmitted on Oct 22 for week of Sept 4-7 \r\nNeeded to review the microbiology of the infection and characteristics of the appropriate antibiotic for the infection.',
              ['Grayson', 'Wilson', 'Lynsey', 'Martin'])
 ]
-nicknames = {"richard": ['rick', 'ricky', 'richie', 'dick'],
-             "stewart": ['stu'],
-             "samuel": ['sam', 'sammy'],
-             "elizabeth": ['elle', 'liz'],
-             "jason": ['jay']
-             }
+nicknames = {}
+
+with open('./nicknames.json', 'r') as file:
+    nicknames = json.loads(file.read())
+
 for test in tests:
     text = test.text
     names = test.names
